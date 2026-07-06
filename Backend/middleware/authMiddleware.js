5d49f507bcedc45cb1df0678d5c5194d1dab31f1
Harsh Vardhan Singh <168753804+Harshvardhan312003@users.noreply.github.com>
2026-07-06 19:35:41 +0530

    Refactor try-catch block in authMiddleware

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const protect = async (req, res, next) => {
   let token;
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
+    try { 
       // Get token from header
       token = req.headers.authorization.split(' ')[1];
 
